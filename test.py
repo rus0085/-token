c04b6d9bbb611e6d388111d5026cd08f54d30327
rus0085 <91891466+rus0085@users.noreply.github.com>
2023-01-15 21:27:41 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-
-connector = sqlite3.connect("BQACAgIAAxkBAAIBCGM9UyRmMOlqbvA8OLWAp1XSisusAAL8GgACxAPpSWq_YYSa3giUKgQ.db")
-cursor = connector.cursor()
-cursor.execute("SELECT * FROM users")
-r = cursor.fetchall()
-print(r)
